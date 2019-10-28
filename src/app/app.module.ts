cf271e487364a1e84f2a77a3ad3a371178c10a52
Tyler Briskie <tylerbriskie@gmail.com>
2019-10-28 15:43:14 -0600

    basic routing working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { BookingComponent } from './booking/booking.component';
+import { LoginComponent } from './login/login.component';
+import { IteneraryComponent } from './itenerary/itenerary.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomepageComponent,
+    BookingComponent,
+    LoginComponent,
+    IteneraryComponent
   ],
   imports: [
     BrowserModule,
