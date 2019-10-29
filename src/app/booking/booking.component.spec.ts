13bab83fd0b5ddd1666c210b3c29355099c7777e
Tyler Briskie <tylerbriskie@gmail.com>
2019-10-29 13:40:40 -0600

    booking component simplified

--- src/app/booking/booking.component.spec.ts
+++ src/app/booking/booking.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookingComponent } from './booking.component';
+import { FormsModule } from '@angular/forms';
 
 describe('BookingComponent', () => {
   let component: BookingComponent;
@@ -8,6 +9,7 @@ describe('BookingComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [FormsModule],
       declarations: [ BookingComponent ]
     })
     .compileComponents();
@@ -17,9 +19,29 @@ describe('BookingComponent', () => {
     fixture = TestBed.createComponent(BookingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    component.currentBooking = {
+      car: false,
+      hotel: false,
+      flight: false,
+    }
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be able to select a car', () => {
+    component.toggleCar();
+    expect(component.currentBooking.car).toBe(true);
+  });
+
+  it('should be able to select a flight', () => {
+    component.toggleFlight();
+    expect(component.currentBooking.flight).toBe(true);
+  });
+
+  it('should be able to select a hotel', () => {
+    component.toggleHotel();
+    expect(component.currentBooking.hotel).toBe(true);
   });
 });
--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -8,38 +8,31 @@ import { TripTypes } from '../core/mockData/TripTypes';
   styleUrls: ['./booking.component.scss']
 })
 export class BookingComponent implements OnInit {
-  selectedTripType: TripType = TripTypes[0];
-  tripTypes: TripType[] = TripTypes;
-  carTypes = ['Economy', 'Luxury', 'SUV', 'Van'];
-  cities = ['Denver', 'Dallas', 'Boston', 'San Francisco', 'Toronto', 'London','New York', 'Los Angeles', 'Tokyo', 'Hamburg', 'Cairo'];
   currentBooking: {
-    startDate: string,
-    endDate: string,
-    origin: string,
-    destination: string,
-    adults: number,
-    children: number,
-    carType: string,
+    car: boolean,
+    flight: boolean,
+    hotel: boolean,
+  } = {
+    car: false,
+    flight: false,
+    hotel: false
   };
-
 
   constructor() { }
 
   ngOnInit() {
-    this.currentBooking = {
-      startDate: "02/03/2020",
-      endDate: "02/06/2020",
-      origin: "Denver",
-      destination: "London",
-      adults: 0,
-      children: 0,
-      carType: 'Economy'
-
-    }
   }
 
-  selectTripType(type: TripType){
-      this.selectedTripType = type;
+  toggleCar(){
+      this.currentBooking.car = !this.currentBooking.car;
   }
 
+  toggleFlight(){
+    this.currentBooking.flight = !this.currentBooking.flight;
+
+  }
+
+  toggleHotel() {
+    this.currentBooking.hotel = !this.currentBooking.hotel;
+  }
 }
